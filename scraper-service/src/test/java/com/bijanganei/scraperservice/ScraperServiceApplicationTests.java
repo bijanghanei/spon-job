b5bb4d296368e92fa0a8535b23520343f1a81d92
bijanghanei <iambijanghanei@gmail.com>
2024-05-08 14:04:11 +0330

    initial comit

---
+++ scraper-service/src/test/java/com/bijanganei/scraperservice/ScraperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bijanganei.scraperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
