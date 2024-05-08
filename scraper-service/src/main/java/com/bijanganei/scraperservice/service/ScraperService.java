aea0c9b1796739926e45a87f023175d440874bcf
bijanghanei <iambijanghanei@gmail.com>
2024-05-08 17:48:30 +0330

    project structure is ready

--- scraper-service/src/main/java/com/bijanganei/scraperservice/service/ScraperService.java
+++ scraper-service/src/main/java/com/bijanganei/scraperservice/service/ScraperService.java
@@ -1,4 +1,16 @@
 package com.bijanganei.scraperservice.service;
 
+import com.bijanganei.scraperservice.entity.JobPost;
+
+import java.util.List;
+import java.util.Set;
+
 public interface ScraperService {
+    boolean checkJobDescription(String url);
+    List<JobPost> extractJobs(String url);
+    List<JobPost> getAllJobs();
+    JobPost getJobById(String id);
+    void removeJobById(String id);
+    void addJob(JobPost job);
+
 }
--- scraper-service/src/main/java/com/bijanganei/scraperservice/service/ScraperServiceImpl.java
+++ scraper-service/src/main/java/com/bijanganei/scraperservice/service/ScraperServiceImpl.java
@@ -1,7 +1,39 @@
 package com.bijanganei.scraperservice.service;
 
+import com.bijanganei.scraperservice.entity.JobPost;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ScraperServiceImpl implements ScraperService{
+    @Override
+    public boolean checkJobDescription(String url) {
+        return false;
+    }
+
+    @Override
+    public List<JobPost> extractJobs(String url) {
+        return null;
+    }
+
+    @Override
+    public List<JobPost> getAllJobs() {
+        return null;
+    }
+
+    @Override
+    public JobPost getJobById(String id) {
+        return null;
+    }
+
+    @Override
+    public void removeJobById(String id) {
+
+    }
+
+    @Override
+    public void addJob(JobPost job) {
+
+    }
 }
